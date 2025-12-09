08a1ca7b4f5810d0ff35fd081293441e3053ef85
T-re-v <richards.83@wright.edu>
2025-12-09 18:31:09 -0500

    bash script

---
+++ Project5/refresh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+CONTAINER_NAME="Project5"
+IMAGE_NAME="spootymcspoot/project3:latest"
+
+echo "Stopping and removing old container (if any)..."
+docker rm -f $CONTAINER_NAME || true
+
+echo "Pulling latest image..."
+docker pull $IMAGE_NAME
+
+echo "Starting new container..."
+docker run -d --name $CONTAINER_NAME -p 8000:80 --restart unless-stopped $IMAGE_NAME
