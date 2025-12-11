9118e720f4ffc54f16dd4bbf03ab2fca55925e55
T-re-v <richards.83@wright.edu>
2025-12-10 22:02:23 -0500

    fixed all webhook stuff

--- Project5/refresh.sh
+++ Project5/refresh.sh
@@ -5,10 +5,10 @@ CONTAINER_NAME="Project5"
 IMAGE_NAME="spootymcspoot/project3:latest"
 
 echo "Stopping and removing old container (if any)..."
-docker rm -f $CONTAINER_NAME || true
+sudo docker rm -f $CONTAINER_NAME || true
 
 echo "Pulling latest image..."
-docker pull $IMAGE_NAME
+sudo docker pull $IMAGE_NAME
 
 echo "Starting new container..."
-docker run -d --name $CONTAINER_NAME -p 8000:80 --restart unless-stopped $IMAGE_NAME
+sudo docker run -d --name $CONTAINER_NAME -p 8000:80 --restart unless-stopped $IMAGE_NAME
